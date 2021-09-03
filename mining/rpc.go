b1d0cb1ecbe6c2d8982b0d737cacb39ac8648ae7
Oleksiy Pyltsov <valar999@gmail.com>
2021-09-03 23:52:42 +0300

    Add rpc initial implementation

--- mining/rpc.go
+++ mining/rpc.go
@@ -1,22 +1,107 @@
 package mining
 
 import (
-	jsonrpc "github.com/Arhius/jsonrpc-codec/jsonrpc1"
+	"log"
+	"context"
+	"time"
+	"gitlab.com/jaxnet/jaxnetd/network/rpcclient"
+	"gitlab.com/jaxnet/jaxnetd/types/jaxjson"
 )
+
+const getTemplateInverval = time.Second
 
 type RPCClient struct {
 	config *Config
-	rpc    *jsonrpc.Client
+	rpc    *rpcclient.Client
+	shards map[uint32]context.CancelFunc
+	log    *log.Logger
 }
 
-func NewRPCClient(config *Config) *RPCClient {
+func NewRPCClient(config *Config) (*RPCClient, error) {
+	rpc, err := rpcclient.New(jaxRPCConfig(config.serverAddress), nil)
+	if err != nil {
+		return nil, err
+	}
 	return &RPCClient{
 		config: config,
-		rpc: jsonrpc.NewHTTPClient(config.serverAddress),
+		rpc: rpc,
+		shards: make(map[uint32]context.CancelFunc),
+		log: log.Default(),
+	}, nil
+}
+
+func (c *RPCClient) fetchShards() {
+	res, err := c.rpc.ListShards()
+	if err != nil {
+		c.log.Println("ERR", err)
+		return
+	}
+	for id, shard := range res.Shards {
+		if !shard.Enabled {
+			continue
+		}
+		if _, ok := c.shards[id]; !ok {
+			ctx, cancel := context.WithCancel(context.Background())
+			c.shards[id] = cancel
+			go c.fetchShardTemplate(ctx, id)
+		}
+	}
+	for id, _ := range c.shards {
+		if _, ok := res.Shards[id]; !ok {
+			// TODO shard deleted
+		}
 	}
 }
 
 func (c *RPCClient) Do() {
 	for {
+		c.fetchShards()
+		time.Sleep(1)
+	}
+}
+
+func (c *RPCClient) fetchShardTemplate(ctx context.Context, id uint32) {
+	clientConfig := jaxRPCConfig(c.config.serverAddress)
+	clientConfig.ShardID = id
+	rpc, err := rpcclient.New(clientConfig, nil)
+	if err != nil {
+		// TODO should we return here?
+		c.log.Println("ERR", err)
+		return
+	}
+
+	params := &jaxjson.TemplateRequest{}
+	for {
+		ch := GetShardBlockTemplateAsync(rpc, params)
+		select {
+		case r := <-ch:
+			if r.err == nil {
+				template := r.result
+				params.LongPollID = template.LongPollID
+				log.Println("shard", id, template.Height)
+				// TODO job.update(template)
+			} else {
+				c.log.Println("ERR", r.err)
+				time.Sleep(getTemplateInverval)
+			}
+		case <-ctx.Done():
+			c.log.Println("stop fetching template shard", id)
+			return
+		}
+	}
+}
+
+func jaxRPCConfig(address string) *rpcclient.ConnConfig {
+	_ = address
+	// TODO
+	return &rpcclient.ConnConfig{
+		Host: "128.199.64.36:18333",
+		Endpoint: "ws",
+		Params: "testnet",
+		User: "jaxnetrpc",
+		Pass: "AUL6VBjoQnhP3bfFzl",
+		// ShardID: 1,
+		HTTPPostMode: true,
+		DisableTLS: true,
 	}
 }
---
+++ mining/rpc_test.go
@@ -0,0 +1,18 @@
+package mining
+
+import (
+	"testing"
+)
+
+const server = "http://jaxnetrpc:AUL6VBjoQnhP3bfFzl@128.199.64.36:18333"
+
+func TestXXX(t *testing.T) {
+	conf := &Config{
+	        serverAddress: server,
+	}
+	client, err := NewRPCClient(conf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	client.Do()
+}
