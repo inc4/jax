17c212df94cc1067ee6731f6e7299cc5cf1441c9
svinerus <svinerus@gmail.com>
2021-09-05 14:48:56 +0300

    Fix typo and style of GetBitcoinCoinbase func

--- mining/job/job.go
+++ mining/job/job.go
@@ -164,21 +164,21 @@ func (h *Job) ProcessBeaconTemplate(template *jaxjson.GetBeaconBlockTemplateResu
 	}
 }
 
-func (h *Job) GetBitcoinCoinbase(reward, fee int64, height uint32) (par1, part2 []byte, err error) {
+func (h *Job) GetBitcoinCoinbase(reward, fee int64, height uint32) (part1, part2 []byte, err error) {
 	jaxCoinbaseTx, err := mining.CreateJaxCoinbaseTx(reward, fee, int32(height), 0, h.config.BtcMiningAddress, h.config.BurnBtcReward)
 	if err != nil {
-		return nil, nil, err
+		return
 	}
 	coinbaseTx := utils.JaxTxToBtcTx(jaxCoinbaseTx.MsgTx())
 
 	coinbaseTx.TxIn[0].SignatureScript, err = utils.BTCCoinbaseScript(int64(height), utils.PackUint64LE(0x00), h.BeaconHash[:])
 	if err != nil {
-		return nil, nil, err
+		return
 	}
 
 	fakeBlock := btcdwire.MsgBlock{Transactions: []*btcdwire.MsgTx{&coinbaseTx}}
-	part1, part2 := utils.SplitCoinbase(&fakeBlock)
-	return part1, part2, err
+	part1, part2 = utils.SplitCoinbase(&fakeBlock)
+	return
 }
 
 func (h *Job) updateMergedMiningProof() (err error) {
