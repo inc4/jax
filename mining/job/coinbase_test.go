ac06e0425a4a38f90fa987bcf5d874bef7fbd9a2
svinerus <svinerus@gmail.com>
2021-09-06 02:47:01 +0300

    Update coinbase_test.go

--- mining/job/coinbase_test.go
+++ mining/job/coinbase_test.go
@@ -2,17 +2,12 @@ package job
 
 import (
 	"encoding/hex"
-	"github.com/inc4/jax/mining/rpc"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestCoinbase(t *testing.T) {
-	rpcClient, err := rpc.NewRPCClient("")
-	if err != nil {
-		t.Fatal(err)
-	}
-	job, _ := NewJob(rpcClient, "", "")
+	job, _ := NewJob("mzDGR33maDBujpqjkvxVzY2ssYDcQG51p3", "mzDGR33maDBujpqjkvxVzY2ssYDcQG51p3")
 
 	coinbase, err := job.GetBitcoinCoinbase(&CoinBaseData{Reward: 625540727, Fee: 666, Height: 703687})
 	if err != nil {
@@ -20,5 +15,5 @@ func TestCoinbase(t *testing.T) {
 	}
 
 	assert.Equal(t, "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff3c03c7bc0a08", hex.EncodeToString(coinbase.Part1))
-	assert.Equal(t, "2000000000000000000000000000000000000000000000000000000000000000000d2f503253482f6a61786e65742fffffffff030000000000000000176a152068747470733a2f2f6a61782e6e6574776f726b2077fe48250000000001519a02000000000000015100000000", hex.EncodeToString(coinbase.Part2))
+	assert.Equal(t, "2000000000000000000000000000000000000000000000000000000000000000000d2f503253482f6a61786e65742fffffffff030000000000000000176a152068747470733a2f2f6a61782e6e6574776f726b2077fe4825000000001976a914cd120759aa39d9184d19b8c390d30da979218cea88ac9a020000000000001976a914cd120759aa39d9184d19b8c390d30da979218cea88ac00000000", hex.EncodeToString(coinbase.Part2))
 }
