c1bd84ba39508778a26aa9e331bb4ca3e12a282a
svinerus <svinerus@gmail.com>
2021-09-06 12:46:00 +0300

    Remove interface

--- mining/miner.go
+++ mining/miner.go
@@ -2,25 +2,13 @@ package mining
 
 import (
 	"github.com/inc4/jax/mining/job"
-	"gitlab.com/jaxnet/jaxnetd/jaxutil"
 	"gitlab.com/jaxnet/jaxnetd/network/rpcclient"
-	"gitlab.com/jaxnet/jaxnetd/types/jaxjson"
 	"net/url"
 )
 
-//go:generate mockgen -source=miner.go -destination mock_job_test.go -package mining
-
-type RpcClient interface {
-	SubmitBlock(*jaxutil.Block, *jaxjson.SubmitBlockOptions) error
-	ForShard(uint32) *rpcclient.Client
-	ForBeacon() *rpcclient.Client
-	ListShards() (*jaxjson.ShardListResult, error)
-	GetBeaconBlockTemplate(*jaxjson.TemplateRequest) (*jaxjson.GetBeaconBlockTemplateResult, error)
-}
-
 type Miner struct {
 	job       *job.Job
-	rpcClient RpcClient
+	rpcClient *rpcclient.Client
 }
 
 func NewMiner(serverAddress, BtcAddress, JaxAddress string) (*Miner, error) {
